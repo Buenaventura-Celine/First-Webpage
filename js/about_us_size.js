10072493bff8d2837d5f3a853f216618ee9824cd
Buenaventura, Celine Joie V <52587198+Buenaventura-Celine@users.noreply.github.com>
2021-04-21 16:25:54 +0800

    Add files via upload

---
+++ js/about_us_size.js
@@ -0,0 +1,36 @@
+if (screen.width == 1768 && screen.height == 992){
+    document.write("<style>body{zoom:90%}</style>");
+}
+else if(screen.width == 1680 && screen.height == 1050){
+    document.write("<style>body{zoom:88%}</style>");
+}
+else if(screen.width == 1600 && screen.height == 1024){
+    document.write("<style>body{zoom:83%}</style>");
+}
+else if(screen.width == 1600 && screen.height == 900){
+    document.write("<style>body{zoom:83%}</style>");
+}
+else if(screen.width == 1440 && screen.height == 900){
+    document.write("<style>body{zoom:75%}</style>");
+}
+else if(screen.width == 1366 && screen.height == 768){
+    document.write("<style>body{zoom:70%}</style>");
+}
+else if(screen.width == 1360 && screen.height == 768){
+    document.write("<style>body{zoom:70%}</style>");
+}
+else if(screen.width == 1280 && screen.height == 768){
+    document.write("<style>body{zoom:67%}</style>");
+}
+else if(screen.width == 1280 && screen.height == 720){
+    document.write("<style>body{zoom:67%}</style>");
+}
+else if(screen.width == 1280 && screen.height == 600){
+    document.write("<style>body{zoom:67%}</style>");
+}
+else if(screen.width == 1024 && screen.height == 768){
+    document.write("<style>body{zoom:95%}</style>");
+}
+else{
+    document.write("<style>body{zoom:100%}</style>");
+}
